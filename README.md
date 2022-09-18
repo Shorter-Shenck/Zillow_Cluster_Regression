39cdcad13b8a46a832672f0e465565ce17fc76cf
Geary Shenck <geary.shenck@gmail.com>
2022-09-18 13:36:10 -0500

    amended: DD,explore,reran

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 > Ask/Answer exploratory questions of data and attributes to understand drivers of home value  
 
-* Utilize charts, statistical tests, and various clustering models with 
+* Utilize charts, statistical tests, and various clustering models to drive linear regression models; improving baseline model
 
 > Construct models to predict `logerror`
 * Log Error: log(zestimate) - log(Saleprice)
@@ -24,7 +24,7 @@
 > Be prepared to answer panel questions about all project areas
 
 ## Project Business Goals
-> Construct ML Regression model that accurately predicts log error of *Single Family Properties* using clustering techniques to guide feature selection </br>
+> Construct ML Regression model that accurately predicts log error of *Single Family Properties* using clustering techniques to guide feature selection for modeling</br>
 
 >Find key drivers of log error</br>
 
@@ -37,69 +37,64 @@
 > 5 minute recording of a presentation of final notebook</br>
 
 ## Data Dictionary **
-|Target|Datatype|Definition|
-|:-----|:-----|:-----|
-|home_value|xxx non-null: uint8| property tax assessed values
+|       Target             |           Datatype       |     Definition      |
+|:-------------------------|:------------------------:|-------------------:|  
+logerror                   | 51736 non-null  float64  | Log(Zestimate) - Log(SalePrice): Difference in estimated and actual
 
-|Feature|Datatype|Definition|
-|:-----|:-----|:-----|
-bathrooms       | 4225 non-null   float64 | number of bathrooms in home
-bedrooms        | 4225 non-null   float64 | number of bedrooms in home
-county          | 4225 non-null   object  | county of home based on FIPS code
-area            | 4225 non-null   float64 | area of home in calculated square feet
-home_size       | 4225 non-null   object | size of home grouped in category by square feet
-home_age        | 4225 non-null   int64  | age of home
-decades         | 4225 non-null   object | decade in which home was constructed
-est_tax_rate    | 4225 non-null   float64 | calculated estimnated tax rate bbased on home value and paid tax
-
+|       Feature            |           Datatype       |     Definition      |
+|:------------------------|:------------------------:|-------------------:|  
+basementsqft               | 51736 non-null  float64  | Square Feet (Sqft) of basement
+bathrooms                  | 51736 non-null  float64  | Count of bathrooms
+bedrooms                   | 51736 non-null  float64  | Count of bedrooms
+area                       | 51736 non-null  float64  | Finished Sqft of Home
+county                     | 51736 non-null  object   | Name of County Home is in
+garagecarcnt               | 51736 non-null  float64  | Count of Cars rated for Garage
+garagetotalsqft            | 51736 non-null  float64  | Sqft of Garage
+latitude                   | 51736 non-null  float64  | Angular distance (East/West) of a home (degrees and minutes)
+longitude                  | 51736 non-null  float64  | Angular distance (North/South) of a home (degrees and minutes)
+lotsizesquarefeet          | 51736 non-null  float64  | Sqft of Lot (land)
+poolcnt                    | 51736 non-null  float64  | Count of Pools
+structuretaxvaluedollarcnt | 51736 non-null  float64  | Tax Value of Structure/Home
+home_value                 | 51736 non-null  float64  | Tax Value of Property
+landtaxvaluedollarcnt      | 51736 non-null  float64  | Tax Value of Land/Lot
+aircon                     | 51736 non-null  object   | Air Conditioning Type
+heating                    | 51736 non-null  object   | Heating Type
+age                        | 51736 non-null  float64  | FE: Years since House Built
+openness                   | 51736 non-null  float64  | FE: Area / (Bedrooms + Bathrooms): Relative size
+tax_per_sqft               | 51736 non-null  float64  | FE: Home Value / Sqft: Relative value
+home_size                  | 51736 non-null  category | FE: Binned Category of Area
+est_tax_rate               | 51736 non-null  float64  | FE: Estimated Tax Rate of County
+cluster house_tax          | 51736 non-null  int32    | FE: Groups of ["tax_per_sqft","est_tax_rate","openness"]
+cluster house_details      | 51736 non-null  int32    | FE: Groups of ["lotsizesquarefeet",'garagetotalsqft',"poolcnt"]
+cluster house_sizing       | 51736 non-null  int32    | FE: Groups of ['area','bathrooms','bedrooms']
+cluster house_locale       | 51736 non-null  int32    | FE: Groups of ["latitude","longitude","age"],
+                    
 
 ## Initial Questions and Hypotheses
-### Question 1
- > Is there a difference in `garagetotlsqrefeet` between different `logerrorbins`?
-* ${\alpha}$ = .05
+### Question 1 -  Does logerror and Area(sqft) have a signifcant relationship
+* ${H_0}$: There is not a significant relationship in LogError and Area 
+* ${H_a}$: There is a significant relationship in LogError and Area 
+> Conclusion: There is enough evidence to reject our null hypothesis.
 
-* ${H_0}$: 
+### Question 2 - Is there a difference in the the sample of no Heating for Log Error?
+* ${H_0}$: There is no significant difference in mean LogError of the sample No Heating compared to the population  
+* ${H_a}$: There is significant difference in mean LogError of the sample No Heating compared to the population  
+> Conclusion: There is enough evidence to reject our null hypothesis.
 
-* ${H_a}$: 
-
- * Conclusion: 
-
-### Question 2
- > Did the k = 3 cluster ('openness') object featuring bedrooms/homesize/bathrooms show difference in logerror mean
- * ${\alpha}$ = .05
-
-* ${H_0}$: 
-
-* ${H_a}$: 
-
- * Conclusion: 
-
- ### Question 3
- > Second cluster - location, age 
-* ${\alpha}$ = .05
-
-* ${H_0}$: 
-
-* ${H_a}$: 
-
- * Conclusion: 
+### Question 3 -  Does the Cluster group of Home Size have any clusters that are significantly different in relation to logerror?
+* ${H_0}$: There is no significant difference in mean LogError of the sample Home Size (Cluster 4) compared to the population  
+* ${H_a}$: There is significant difference in mean LogError of the sample Home Size (Cluster 4) compared to the population  
+> Conclusion: There is enough evidence to reject our null hypothesis. 
  
- ### Question 4 
- >
-* ${\alpha}$ = .05
-
-* ${H_0}$: 
-
-* ${H_a}$: 
-
- * Conclusion: 
+### Question 4 -  Does the Cluster group of House Locale have any clusters that are significantly different in relation to logerror?
+* ${H_0}$: There is no significant difference in mean LogError of the sample House Locale (Cluster 1) compared to the population  
+* ${H_a}$: There is significant difference in mean LogError of the sample House Locale (Cluster 1) compared to the population  
+> Result: There is enough evidence to reject our null hypothesis.
 
 
 ## Summary of Key Findings and Takeaways
-* 
-* 
-* 
-* 
+> - A combination of features and clusters will likely give us the best result to predict log error
+> - These features may include: Area, Heating, Cluster of Home Size Area(sqft), Bedroom, Bathroom, Home Locale, Location, and age will all prove to have a level of significance in helping predict our target variable of log error in our modeling
 
 ## Pipeline Walkthrough
 ### Plan
@@ -112,11 +107,6 @@ est_tax_rate    | 4225 non-null   float64 | calculated estimnated tax rate bbase
 > ~~include or remove outliers for exploration~~
 > Explore
 > Clustering stuff
-* A cluster model: 
-    - k=3: square feet, bathroom, bedrooms
-    - k=3: pool, garage, acreage
-    - k=3: estimated tax rate, openness value, land price per square foot
-    - k=3:  
 > Statistical testing based on clustering 
 > Modeling stuff 
 * Handle acquire, explore, and scaling in wrangle
@@ -134,16 +124,20 @@ est_tax_rate    | 4225 non-null   float64 | calculated estimnated tax rate bbase
 > Handle any possible threats of data leakage
 
 > Feature Engineering *shifted to accomodate removal of outliers*
-* Decades: Columns featuring the decade in which the home was 
 * Age: Columns that have the Age 
 * Size: Column created to categorize homes by size
 * Openness: Column created to measure the 'opennes'
 * Estimated Tax Rate: Created to estimate a tax rate based off the home_value divided by the tax rate
-* Some calculation involving tax value vs lotsize and structure size/taxsssssss......
+* A cluster model: 
+> - house_tax :|: [tax_per_sqft,est_tax_rate,openness]
+> - house_details :|: [lotsizesquarefeet,garagetotalsqft,poolcnt]
+> - house_sizing :|: [area,bathrooms,bedrooms]
+> - house_locale :|: [latitude,longitude,age]
+
 > Split data
 > Scale data
 > Collect and collate section *Takeaways*
-> Add appropirate artifacts into `wrangle.py`
+> Add appropirate artifacts into `wrangle.py` or `explore.py`
 
 ### Explore
 * Removed Year Built, and Tax Amount
@@ -182,6 +176,6 @@ est_tax_rate    | 4225 non-null   float64 | calculated estimnated tax rate bbase
 > Requires personal `env.py` file containing database credentials  
 > Steps:
 * Fully examine this `README.md`
-* Download `acquire.py, model.py, prepary.py, and final_report.ipynb` to working directory
+* Download `acquire.py, explore.py, model.py, prepary.py, and final_report.ipynb` to working directory
 * Create and add personal `env.py` file to directory. Requires user, password, and host variables
 * Run ``final_report.ipynb`
---
+++ explore_zillow.py
@@ -0,0 +1,219 @@
+#### Import Section
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import env
+import wrangle_zillow
+from os.path import exists
+
+from itertools import product
+from scipy.stats import levene , pearsonr, spearmanr, mannwhitneyu, f_oneway, ttest_ind,ttest_1samp
+from sklearn.metrics import mean_squared_error, explained_variance_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression, TweedieRegressor, LassoLars
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import RFE, f_regression, SelectKBest
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# expore init
+def explore_inital_guidance(train):
+    """  
+    takes in train and creates a previous made list of features to heatmap in the guidance of exploration
+    """
+    #list to heatmatp corr values
+    heatmap_list = ['basementsqft','area','garagetotalsqft','latitude','longitude',
+                    'lotsizesquarefeet','structuretaxvaluedollarcnt','home_value',
+                    'landtaxvaluedollarcnt','logerror','age','home_size','est_tax_rate',
+                    "bathrooms","bedrooms","openness"]
+
+    #Creates and plotts the correlation values
+    fig,ax = plt.subplots(figsize=(18,12))
+
+    kwargs = {'alpha':1,'linewidth':5,'linestyle':'--','linecolor':'white'}
+
+    sns.heatmap(train[heatmap_list].corr(),
+                cmap="Spectral",mask=(np.triu(np.ones_like(train[heatmap_list].corr(),
+                dtype=bool))),annot=True,vmin=-1, vmax=1)
+    ax.add_patch(patches.Rectangle((9, 10),1.0,35.0,edgecolor='red',fill=False,lw=4) )
+    ax.add_patch(patches.Rectangle((9,9),-10.0,1.0,edgecolor='red',fill=False,lw=4) )
+    plt.xticks(rotation = 60)
+    plt.title("Are there features that correlate higher than others?")
+    plt.show()
+
+
+# q2 explore heating varible
+def explore_heating(train):
+    ''' 
+    specific made function, takes in train and runs a tailored function that 
+    does a 1 sample ttest based on predefined expectations and plots a the sub-cat in relation to non sub-cat for that feature
+    '''
+    # sets variables
+    target = "logerror"
+    alpha = .05
+    population_name = "heating"
+    sample_name = "None"
+    
+    #sets null hypothesis
+    H0 = f"{sample_name} as a sample has equal mean values to {population_name} as a population regarding {target}"
+    Ha = f"{sample_name} as a sample does not have equal mean values to {population_name} as a population regarding {target}"
+
+    #runs test and prints results
+    t, p = ttest_1samp( train[train[population_name] == sample_name][target], train[target].mean())
+    if p > alpha:
+        print("\n We fail to reject the null hypothesis (",(H0) , ")",'t=%.5f, p=%.5f' % (t,p))
+    else:
+        print("\n We reject the null Hypothesis (", '\u0336'.join(H0) + '\u0336' ,")",'t=%.5f, p=%.5f' % (t,p))
+
+    #creates a temp df that assists in plotting the feature
+    temp1 = train.copy()
+    temp1["Heating Compared"] = np.where(temp1[population_name] == sample_name,"Heating - No", "Heating - Yes")
+
+    #does the plotting
+    plt.figure(figsize=(12,6))
+    sns.barplot(
+        data=temp1, x="Heating Compared", y="logerror",
+        linewidth=3, edgecolor=".5", facecolor=(0, 0, 0, 0),)
+    plt.axhline(y=temp1.logerror.mean(),label=f"LogError Mean {round(temp1.logerror.mean(),3)}",color="black")
+    plt.legend()
+    plt.title("Heating Compared in relation to LogError")
+    plt.show()
+
+#q3 house size cluster
+
+def explore_house_size_clusters(train):
+    ''' 
+    specific made function, takes in train and runs a tailored function that 
+    does a 1 sample ttest based on predefined expectations and plots a the sub-cat in relation to non sub-cat for that feature
+    '''
+    # sets variables
+    target = "logerror"
+    alpha = .05
+    population_name = "cluster house_sizing"
+    sample_name = 4
+    H0 = f"{sample_name} as a sample has equal mean values to {population_name} as a population regarding {target}"
+    Ha = f"{sample_name} as a sample does not have equal mean values to {population_name} as a population regarding {target}"
+    
+    #runs test and prints results
+    t, p = ttest_1samp( train[train[population_name] == sample_name][target], train[target].mean())
+    if p > alpha:
+        print("\n We fail to reject the null hypothesis (",(H0) , ")",'t=%.5f, p=%.5f' % (t,p))
+    else:
+        print("\n We reject the null Hypothesis (", '\u0336'.join(H0) + '\u0336' ,")",'t=%.5f, p=%.5f' % (t,p))
+    
+    #creates a temp df that assists in plotting the feature
+    temp1 = train.copy()
+    temp1["House Sizing Clusters"] = np.where(temp1[population_name] == sample_name,"House Sizeing (cluster-4)", "House Sizeing (cluster-others)")
+    #does the plotting
+    plt.figure(figsize=(12,6))
+    sns.barplot(
+        data=temp1, x="House Sizing Clusters", y="logerror",
+        linewidth=3, edgecolor=".5", facecolor=(0, 0, 0, 0),)
+    plt.axhline(y=temp1.logerror.mean(),label=f"LogError Mean {round(temp1.logerror.mean(),3)}",color="black")
+    plt.legend()
+    plt.title("House Size Clusters in relation to LogError")
+    plt.show()
+
+# Q4 house locale cluster
+
+def explore_house_locale_cluster(train):    
+    ''' 
+    specific made function, takes in train and runs a tailored function that 
+    does a 1 sample ttest based on predefined expectations and plots a the sub-cat in relation to non sub-cat for that feature
+    '''
+    # sets variables
+    target = "logerror"
+    alpha = .05
+    population_name = 'cluster house_locale'
+    sample_name = 1
+    
+    #sets null hypothesis
+    H0 = f"{sample_name} as a sample has equal mean values to {population_name} as a population regarding {target}"
+    Ha = f"{sample_name} as a sample does not have equal mean values to {population_name} as a population regarding {target}"
+
+    #runs test and prints results
+    t, p = ttest_1samp( train[train[population_name] == sample_name][target], train[target].mean())
+    if p > alpha:
+        print("\n We fail to reject the null hypothesis (",(H0) , ")",'t=%.5f, p=%.5f' % (t,p))
+    else:
+        print("\n We reject the null Hypothesis (", '\u0336'.join(H0) + '\u0336' ,")",'t=%.5f, p=%.5f' % (t,p))
+
+    #creates a temp df that assists in plotting the feature
+    temp1 = train.copy()
+    temp1["House Locale Compared"] = np.where(temp1[population_name] == sample_name,"Cluster 1", "Other Clusters")
+
+    #does the plotting
+    plt.figure(figsize=(12,6))
+    sns.barplot(
+        data=temp1, x="House Locale Compared", y="logerror",
+        linewidth=3, edgecolor=".5", facecolor=(0, 0, 0, 0),
+    )
+    plt.axhline(y=temp1.logerror.mean(),label=f"LogError Mean {round(temp1.logerror.mean(),3)}",color="black")
+    plt.legend()
+    plt.title("House Locale Compared Compared in relation to LogError")
+    plt.show()
+
+#aux score cluster
+
+def score_clusters(train,target):
+    '''
+    takes in your three sets and runs one sided ttest on the cluster groups to see if they were 
+    averaging significantly different averages
+    '''
+
+    #creates a data frame, to append the iteration of result from 1 sample to population ttest result of clustering
+
+    cluster_test_results = pd.DataFrame(columns=['test', 'population', "variable", "comparing on", "test_value", "p_val"])
+    alpha = .05
+
+    #loops through columns to find any with the name cluster in it
+    for i in train.columns[train.columns.to_series().str.contains('cluster')].tolist():
+
+        #sets the population name to the feature/column name
+        population_name = f"{i}"
+        #sets the sample name to the next unique name in the feature/column
+        for sample_name in train[population_name].unique():
+    
+            t, p = ttest_1samp( train[train[population_name] == sample_name][target], train[target].mean())
+
+            cluster_test_results.loc[len(cluster_test_results.index)] = ["One Sample TTest - 2 sided", 
+                                                                f"{population_name}",
+                                                                f"{population_name}({sample_name})",
+                                                                target,
+                                                                t,
+                                                                p]       
+  
+    #puts out sorted dataframe                              
+    cluster_test_results.sort_values(by=["p_val"])
+    return(cluster_test_results)
+
+def viz_clusters(train,target):
+    '''  
+    takes in dataframe and target string
+    visualizes features with cluster in the name, tailored for single use case
+    '''
+    #plots a violin plot of the clusters for quick review
+    cluster_features = train.columns[train.columns.to_series().str.contains('cluster')].tolist()
+    l = 0
+    plt.figure(figsize=(12,24))
+    for cluster in cluster_features:
+        l += 1
+        plt.subplot(len(cluster_features),2,l)
+        plot_order = train[cluster].sort_values(ascending=True).unique()
+        sns.violinplot(    x=train[cluster], 
+                        y=train[target], 
+                        data=train, 
+                        order = plot_order,
+                        color ="grey",
+                        inner="box"
+                        #notch=True,
+                        )
+        plt.axhline(train[target].mean(),label=f"mean line - {round(train[target].mean(),0)}")
+        plt.ylim([-.25,.25])
+        plt.legend()
+        plt.title(f"value of {target} sorted by {cluster}")
+    plt.show() 
